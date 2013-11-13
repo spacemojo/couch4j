0f7fde60040c337200ace848c29ad9b5165f6663
Frederic Leclerc <fredericleclerc@aheeva.com>
2013-11-13 17:00:10 -0500

    Messy but ongoing development of json removal ...

--- src/main/java/com/standardstate/couch4j/util/Utils.java
+++ src/main/java/com/standardstate/couch4j/util/Utils.java
@@ -3,10 +3,15 @@ package com.standardstate.couch4j.util;
 import com.standardstate.couch4j.Session;
 import java.io.IOException;
 import java.io.StringWriter;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import org.codehaus.jackson.map.ObjectMapper;
 
 public class Utils {
 
+    private final static Pattern REV_INSIDE_PTN = Pattern.compile("(\"_rev\"[\\s]*:[\\s]*\".*\",)");
+    private final static Pattern REV_END_PTN = Pattern.compile("(\"_rev\"[\\s]*:[\\s]*\".*\"})");
+    
     public static String objectToJSON(final Object object) {
         final ObjectMapper mapper = new ObjectMapper();
         final StringWriter writer = new StringWriter();
@@ -20,9 +25,14 @@ public class Utils {
     
     public static String removeRev(final String json) {
         
+        final StringBuffer insideBuffer = new StringBuffer();        
+        final Matcher insideMatcher = REV_INSIDE_PTN.matcher(json);
+        while(insideMatcher.find()) {
+            insideMatcher.appendReplacement(insideBuffer, "");
+        }
+        insideMatcher.appendTail(insideBuffer);
         
-        
-        return json;
+        return insideBuffer.toString();
         
     }
     
--- src/test/java/com/standardstate/couch4j/util/UtilsTest.java
+++ src/test/java/com/standardstate/couch4j/util/UtilsTest.java
@@ -1,19 +1,23 @@
 package com.standardstate.couch4j.util;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 import org.junit.Test;
 
 public class UtilsTest {
     
+    final String[] testJSONs = {"{\"_id\":\"1234567890\", \"_rev\": \"123\", \"name\":\"something\"}", 
+                                "{\"_rev\" :\"123\", \"name\":\"something\"}",
+                                "{\"_id\":\"1234567890\",\"_rev\":\"value\", \"name\":\"something\"}",
+                                "{\"_id\":\"1234567890\", \"_rev\" : \"123\", \"name\":\"something\"}",
+                                "{\"_id\":\"1234567890\", \"_rev\":\"\", \"name\":\"something\"}",
+                                "{\"_id\":\"1234567890\", \"_rev\":\"\", \"name\":\"something\", \"_rev\":\"last ... \"}",
+                                "{\"_id\":\"1234567890\", \"name\":\"something\", \"_rev\":\"last ... \"} "};
+    
     @Test
     public void removeRevTest() {
-        
-        final String test01 = "{\"_id\":\"1234567890\", \"_rev\":\"123\", \"name\":\"something\"}";
-        
-        final Pattern pattern = Pattern.compile("^.?");
-        final Matcher matcher = pattern.matcher(test01);
-        System.out.println(matcher.find());
+                
+        for(String test : testJSONs) {
+            System.out.println(test + " -> " + Utils.removeRev(test));
+        }
         
     }
     
