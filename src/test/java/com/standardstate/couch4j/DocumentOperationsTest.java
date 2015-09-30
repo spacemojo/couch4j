c3a0916546e7f4fcae8c8d268fb0c9b9dcfd82e7
Fred Leclerc <fred.leclerc@gmail.com>
2015-09-30 14:05:55 -0400

    AWS Tests

--- src/test/java/com/standardstate/couch4j/DocumentOperationsTest.java
+++ src/test/java/com/standardstate/couch4j/DocumentOperationsTest.java
@@ -16,191 +16,191 @@ import org.junit.Test;
 @Ignore
 public class DocumentOperationsTest extends BaseCouch4JTest {
 
-    @BeforeClass
-    @Ignore
-    public static void createTestDatabase() {
-        final OperationResponse createResponse = DatabaseOperations.createDatabase(TEST_DATABASE_NAME);
-        assertEquals("createTestDatabase", true, createResponse.isOk());
-    }
-
-    @AfterClass
-    @Ignore
-    public static void deleteTestDatabase() {
-        final OperationResponse deleteResponse = DatabaseOperations.deleteDatabase(TEST_DATABASE_NAME);
-        assertEquals("deleteTestDatabase", true, deleteResponse.isOk());
-    }
-
-    @Test
-    @Ignore
-    public void constructorTest() {
-        assertNotNull("constructorTest()", new DocumentOperations());
-    }
-
-    @Test
-    @Ignore
-    public void createAndDeleteDocumentWithId() {
-
-        final DateTime now = new DateTime();
-        final MockObject mock = new MockObject();
-        mock.setActive(Boolean.TRUE);
-        mock.setDate(now);
-        mock.setIntValue(12);
-        mock.setName("This is the name of the mock bean ... ");
-        mock.set_id("1029384756");
-
-        final OperationResponse create = DocumentOperations.createDocumentWithId(mock, mock.get_id());
-        mock.set_rev(create.getRev());
-        assertTrue("createAndDeleteDocumentWithId", create.isOk());
-
-        final MockObject fetched = DocumentOperations.getDocument(mock.get_id(), MockObject.class);
-        assertEquals("createAndDeleteDocumentWithId", mock, fetched);
-
-        final OperationResponse deleteDocument = DocumentOperations.deleteDocument(mock);
-        assertTrue("createAndDeleteDocumentWithId", deleteDocument.isOk());
-
-    }
-
-    @Test
-    @Ignore
-    public void createAndDeleteDocument() {
-
-        final DateTime now = new DateTime();
-        final MockObject mock = new MockObject();
-        mock.setActive(Boolean.TRUE);
-        mock.setDate(now);
-        mock.setIntValue(12);
-        mock.setName("This is the name of the mock bean ... ");
-
-        final OperationResponse createResponse = DocumentOperations.createDocument(mock);
-        assertEquals("createAndDeleteDocument", true, createResponse.isOk());
-        mock.set_id(createResponse.getId());
-        mock.set_rev(createResponse.getRev());
-
-        final MockObject fetched = DocumentOperations.getDocument(mock.get_id(), MockObject.class);
-        assertEquals("createAndDeleteDocument", mock, fetched);
-
-        final OperationResponse deleteDocument = DocumentOperations.deleteDocument(mock);
-        assertTrue("createAndDeleteDocument", deleteDocument.isOk());
-
-    }
-
-    @Test(expected = RuntimeException.class)
-    @Ignore
-    public void getNonExistingDocument() {
-
-        final MockObject document = DocumentOperations.getDocument("nonexisting", MockObject.class);
-        fail("This test should have failed ... " + document);
-
-    }
-
-    @Test(expected = RuntimeException.class)
-    @Ignore
-    public void createDocumentTwice() {
-
-        final DateTime now = new DateTime();
-        final MockObject mock = new MockObject();
-        mock.setActive(Boolean.TRUE);
-        mock.setDate(now);
-        mock.setIntValue(12);
-        mock.setName("This is the name of the mock bean ... ");
-
-        final OperationResponse createResponse = DocumentOperations.createDocument(mock);
-        mock.set_id(createResponse.getId());
-        mock.set_rev(createResponse.getRev());
-
-        final OperationResponse createTwiceResponse = DocumentOperations.createDocumentWithId(mock, mock.get_id());
-        fail("This test shoudl have failed : " + createTwiceResponse.toString());
-
-    }
-
-    @Test
-    @Ignore
-    public void updateDocument() {
-
-        final DateTime now = new DateTime();
-        final MockObject mock = new MockObject();
-        mock.setActive(Boolean.TRUE);
-        mock.setDate(now);
-        mock.setIntValue(12);
-        mock.setName("This is the name of the mock bean ... ");
-
-        final OperationResponse createResponse = DocumentOperations.createDocument(mock);
-        mock.set_id(createResponse.getId());
-        mock.set_rev(createResponse.getRev());
-
-        mock.setName("This is an UPDATED name .. ");
-        final OperationResponse updateResponse = DocumentOperations.updateDocument(mock);
-        assertTrue(updateResponse.isOk());
-
-        final MockObject document = DocumentOperations.getDocument(mock.get_id(), MockObject.class);
-        assertEquals("updateDocument()", document.get_id(), mock.get_id());
-        assertEquals("updateDocument()", document.getName(), mock.getName());
-
-    }
-
-    @Test
-    @Ignore
-    public void attachmentTest() throws IOException {
-
-        final DateTime now = new DateTime();
-        final MockObject mock = new MockObject();
-        mock.setActive(Boolean.TRUE);
-        mock.setDate(now);
-        mock.setIntValue(12);
-        mock.setName("This is the name of the mock bean ... ");
-
-        final OperationResponse createResponse = DocumentOperations.createDocument(mock);
-
-        final MockObject fetched = DocumentOperations.getDocument(createResponse.getId(), MockObject.class);
-
-        final File file = new File("src/test/resources/com/standardstate/couch4j/dog.jpg");
-
-        final OperationResponse addResponse = DocumentOperations.addAttachment(fetched, file);
-        assertTrue("addResponse", addResponse.isOk());
-
-        final MockObject withAttachments = DocumentOperations.getDocument(createResponse.getId(), MockObject.class);
-        assertEquals("fetchedWithAttachments", 1, withAttachments.get_attachments().size());
-
-        final Attachment attachment = withAttachments.get_attachments().get("dog.jpg");
-        assertEquals("ContentType", "image/jpeg", attachment.getContent_type());
-        assertEquals("Length", 128642, attachment.getLength());
-        assertEquals("Revpos", 2, attachment.getRevpos());
-        assertTrue("Stub", attachment.isStub());
-
-        withAttachments.setName("Yet another name ... ");
-        DocumentOperations.updateDocument(withAttachments);
-
-        final MockObject modifiedWithAttachments = DocumentOperations.getDocument(createResponse.getId(), MockObject.class);
-        final OperationResponse deleteResponse = DocumentOperations.deleteAttachment(modifiedWithAttachments, "dog.jpg");
-        assertTrue("deleteResponse", deleteResponse.isOk());
-
-    }
-
-    @Test
-    @Ignore
-    public void getAllDocuments() throws IOException {
-
-        final int startsize = DocumentOperations.getAllDocuments().getRows().size();
-
-        for (int i = 0; i < 100; i++) {
-            final MockObject mock = new MockObject();
-            mock.setActive(Boolean.TRUE);
-            mock.setDate(new DateTime());
-            mock.setIntValue(i);
-            mock.setName("This is the name of the mock bean ... " + i);
-            DocumentOperations.createDocument(mock);
-        }
-
-        final AllDocuments allDocuments = DocumentOperations.getAllDocuments();
-        assertEquals("getAllDocuments", (startsize + 100), allDocuments.getRows().size());
-        assertEquals("getAllDocuments", new Integer((startsize + 100)), allDocuments.getTotalRows());
-        assertEquals("getAllDocuments", new Integer(0), allDocuments.getOffset());
-
-        final Options options = new Options();
-        options.setLimit(12);
-        final AllDocuments twelveDocuments = DocumentOperations.getAllDocuments(options);
-        assertEquals("twelveDocuments", 12, twelveDocuments.getRows().size());
-
-    }
+//    @BeforeClass
+//    @Ignore
+//    public static void createTestDatabase() {
+//        final OperationResponse createResponse = DatabaseOperations.createDatabase(TEST_DATABASE_NAME);
+//        assertEquals("createTestDatabase", true, createResponse.isOk());
+//    }
+//
+//    @AfterClass
+//    @Ignore
+//    public static void deleteTestDatabase() {
+//        final OperationResponse deleteResponse = DatabaseOperations.deleteDatabase(TEST_DATABASE_NAME);
+//        assertEquals("deleteTestDatabase", true, deleteResponse.isOk());
+//    }
+//
+//    @Test
+//    @Ignore
+//    public void constructorTest() {
+//        assertNotNull("constructorTest()", new DocumentOperations());
+//    }
+//
+//    @Test
+//    @Ignore
+//    public void createAndDeleteDocumentWithId() {
+//
+//        final DateTime now = new DateTime();
+//        final MockObject mock = new MockObject();
+//        mock.setActive(Boolean.TRUE);
+//        mock.setDate(now);
+//        mock.setIntValue(12);
+//        mock.setName("This is the name of the mock bean ... ");
+//        mock.set_id("1029384756");
+//
+//        final OperationResponse create = DocumentOperations.createDocumentWithId(mock, mock.get_id());
+//        mock.set_rev(create.getRev());
+//        assertTrue("createAndDeleteDocumentWithId", create.isOk());
+//
+//        final MockObject fetched = DocumentOperations.getDocument(mock.get_id(), MockObject.class);
+//        assertEquals("createAndDeleteDocumentWithId", mock, fetched);
+//
+//        final OperationResponse deleteDocument = DocumentOperations.deleteDocument(mock);
+//        assertTrue("createAndDeleteDocumentWithId", deleteDocument.isOk());
+//
+//    }
+//
+//    @Test
+//    @Ignore
+//    public void createAndDeleteDocument() {
+//
+//        final DateTime now = new DateTime();
+//        final MockObject mock = new MockObject();
+//        mock.setActive(Boolean.TRUE);
+//        mock.setDate(now);
+//        mock.setIntValue(12);
+//        mock.setName("This is the name of the mock bean ... ");
+//
+//        final OperationResponse createResponse = DocumentOperations.createDocument(mock);
+//        assertEquals("createAndDeleteDocument", true, createResponse.isOk());
+//        mock.set_id(createResponse.getId());
+//        mock.set_rev(createResponse.getRev());
+//
+//        final MockObject fetched = DocumentOperations.getDocument(mock.get_id(), MockObject.class);
+//        assertEquals("createAndDeleteDocument", mock, fetched);
+//
+//        final OperationResponse deleteDocument = DocumentOperations.deleteDocument(mock);
+//        assertTrue("createAndDeleteDocument", deleteDocument.isOk());
+//
+//    }
+//
+//    @Test(expected = RuntimeException.class)
+//    @Ignore
+//    public void getNonExistingDocument() {
+//
+//        final MockObject document = DocumentOperations.getDocument("nonexisting", MockObject.class);
+//        fail("This test should have failed ... " + document);
+//
+//    }
+//
+//    @Test(expected = RuntimeException.class)
+//    @Ignore
+//    public void createDocumentTwice() {
+//
+//        final DateTime now = new DateTime();
+//        final MockObject mock = new MockObject();
+//        mock.setActive(Boolean.TRUE);
+//        mock.setDate(now);
+//        mock.setIntValue(12);
+//        mock.setName("This is the name of the mock bean ... ");
+//
+//        final OperationResponse createResponse = DocumentOperations.createDocument(mock);
+//        mock.set_id(createResponse.getId());
+//        mock.set_rev(createResponse.getRev());
+//
+//        final OperationResponse createTwiceResponse = DocumentOperations.createDocumentWithId(mock, mock.get_id());
+//        fail("This test shoudl have failed : " + createTwiceResponse.toString());
+//
+//    }
+//
+//    @Test
+//    @Ignore
+//    public void updateDocument() {
+//
+//        final DateTime now = new DateTime();
+//        final MockObject mock = new MockObject();
+//        mock.setActive(Boolean.TRUE);
+//        mock.setDate(now);
+//        mock.setIntValue(12);
+//        mock.setName("This is the name of the mock bean ... ");
+//
+//        final OperationResponse createResponse = DocumentOperations.createDocument(mock);
+//        mock.set_id(createResponse.getId());
+//        mock.set_rev(createResponse.getRev());
+//
+//        mock.setName("This is an UPDATED name .. ");
+//        final OperationResponse updateResponse = DocumentOperations.updateDocument(mock);
+//        assertTrue(updateResponse.isOk());
+//
+//        final MockObject document = DocumentOperations.getDocument(mock.get_id(), MockObject.class);
+//        assertEquals("updateDocument()", document.get_id(), mock.get_id());
+//        assertEquals("updateDocument()", document.getName(), mock.getName());
+//
+//    }
+//
+//    @Test
+//    @Ignore
+//    public void attachmentTest() throws IOException {
+//
+//        final DateTime now = new DateTime();
+//        final MockObject mock = new MockObject();
+//        mock.setActive(Boolean.TRUE);
+//        mock.setDate(now);
+//        mock.setIntValue(12);
+//        mock.setName("This is the name of the mock bean ... ");
+//
+//        final OperationResponse createResponse = DocumentOperations.createDocument(mock);
+//
+//        final MockObject fetched = DocumentOperations.getDocument(createResponse.getId(), MockObject.class);
+//
+//        final File file = new File("src/test/resources/com/standardstate/couch4j/dog.jpg");
+//
+//        final OperationResponse addResponse = DocumentOperations.addAttachment(fetched, file);
+//        assertTrue("addResponse", addResponse.isOk());
+//
+//        final MockObject withAttachments = DocumentOperations.getDocument(createResponse.getId(), MockObject.class);
+//        assertEquals("fetchedWithAttachments", 1, withAttachments.get_attachments().size());
+//
+//        final Attachment attachment = withAttachments.get_attachments().get("dog.jpg");
+//        assertEquals("ContentType", "image/jpeg", attachment.getContent_type());
+//        assertEquals("Length", 128642, attachment.getLength());
+//        assertEquals("Revpos", 2, attachment.getRevpos());
+//        assertTrue("Stub", attachment.isStub());
+//
+//        withAttachments.setName("Yet another name ... ");
+//        DocumentOperations.updateDocument(withAttachments);
+//
+//        final MockObject modifiedWithAttachments = DocumentOperations.getDocument(createResponse.getId(), MockObject.class);
+//        final OperationResponse deleteResponse = DocumentOperations.deleteAttachment(modifiedWithAttachments, "dog.jpg");
+//        assertTrue("deleteResponse", deleteResponse.isOk());
+//
+//    }
+//
+//    @Test
+//    @Ignore
+//    public void getAllDocuments() throws IOException {
+//
+//        final int startsize = DocumentOperations.getAllDocuments().getRows().size();
+//
+//        for (int i = 0; i < 100; i++) {
+//            final MockObject mock = new MockObject();
+//            mock.setActive(Boolean.TRUE);
+//            mock.setDate(new DateTime());
+//            mock.setIntValue(i);
+//            mock.setName("This is the name of the mock bean ... " + i);
+//            DocumentOperations.createDocument(mock);
+//        }
+//
+//        final AllDocuments allDocuments = DocumentOperations.getAllDocuments();
+//        assertEquals("getAllDocuments", (startsize + 100), allDocuments.getRows().size());
+//        assertEquals("getAllDocuments", new Integer((startsize + 100)), allDocuments.getTotalRows());
+//        assertEquals("getAllDocuments", new Integer(0), allDocuments.getOffset());
+//
+//        final Options options = new Options();
+//        options.setLimit(12);
+//        final AllDocuments twelveDocuments = DocumentOperations.getAllDocuments(options);
+//        assertEquals("twelveDocuments", 12, twelveDocuments.getRows().size());
+//
+//    }
 
 }
